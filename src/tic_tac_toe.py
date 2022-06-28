d811130b075b78b4631722827dd86ff178d9d6cb
a017545574 <a01754574@itesm.mx>
2022-06-28 13:24:26 -0500

    Update

---
+++ src/tic_tac_toe.py
@@ -0,0 +1,21 @@
+""" 
+
+Tic Tac Toe with UI and IA in Python 
+
+@Created by Elyayoveloz June 28 2022
+
+
+"""
+
+import random
+import os
+import tkinter as tk
+import sys
+from tkinter import *
+from functools import partial
+from copy import deepcopy
+
+
+os.system("clear")
+
+
